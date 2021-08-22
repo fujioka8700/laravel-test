361b10559ee77a5c70daaa84bead5a84242fefce
fujioka8700 <fujioka8700@gmail.com>
2021-08-22 10:46:42 +0900

    [fix] Databaseのテスト

---
+++ tests/Feature/Database/DatabaseTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Feature\Database;
+
+use App\Models\Book;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Schema;
+use Tests\TestCase;
+
+class DatabaseTest extends TestCase
+{
+    // use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // $this->artisan('config:clear');
+    }
+
+    public function testDatabase()
+    {
+        $this->assertTrue(
+            Schema::hasColumns('books', [
+                'id', 'title', 'author'
+            ])
+        );
+    }
+
+    public function testSaveDatabase()
+    {
+        $book = new Book();
+        $book->title = 'hoge';
+        $book->author = 'tarou';
+        $saveBook = $book->save();
+
+        $this->assertTrue($saveBook);
+    }
+
+    public function testRegisterDatabase()
+    {
+        $book = [
+            'title' => 'hoge'
+        ];
+        $this->assertDatabaseHas('books', $book);
+    }
+}
