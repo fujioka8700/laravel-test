8c735c1ff540ce382c576c5c796aab4e3185ccf3
fujioka8700 <fujioka8700@gmail.com>
2021-08-26 00:48:26 +0900

    [fix] ストップウォッチのリファクタリング

--- resources/views/timecounter/index.blade.php
+++ resources/views/timecounter/index.blade.php
@@ -9,127 +9,78 @@
 </head>
 
 <body>
-    <div id="timer">00:00:000</div>
-    <button id="start">start</button>
-    <button id="stop">stop</button>
-    <button id="reset">reset</button>
-
+    <form name="myForm">
+        <input type="text" size="28" name="myFormTime">
+        <input type="button" value="Start" name="myFormButton" onclick="myCheck()">
+    </form>
     <script>
-        class name {
-            let a = 200;
-          constructor () {
-              this.a = 100;
-          }
-          get aaa(){
-              console.log(this.a);
-          }
+
+        // ストップウォッチの状態。
+        Status = {
+            'TIMERSTART' : 0,
+            'TIMERSTOP'  : 1
         }
-        new name().aaa();
 
-        (function () {
-            'use strict';
+        toggleButton = Status.TIMERSTART;
 
-            //htmlのidからデータを取得
-            //取得したデータを変数に代入
+        function myCheck() {
+            // 計測開始
+            // StartからStopに表示変更
+            // 計測停止
+            // StopからStartに表示変更
+            // 0秒に戻す
 
-            var timer = document.getElementById('timer');
-            var start = document.getElementById('start');
-            var stop = document.getElementById('stop');
-            var reset = document.getElementById('reset');
-
-            //クリック時の時間を保持するための変数定義
-            var startTime;
-
-            //経過時刻を更新するための変数。 初めはだから0で初期化
-            var elapsedTime = 0;
-
-            //タイマーを止めるにはclearTimeoutを使う必要があり、そのためにはclearTimeoutの引数に渡すためのタイマーのidが必要
-            var timerId;
-
-            //タイマーをストップ -> 再開させたら0になってしまうのを避けるための変数。
-            var timeToadd = 0;
-
-
-            //ミリ秒の表示ではなく、分とか秒に直すための関数, 他のところからも呼び出すので別関数として作る
-            //計算方法として135200ミリ秒経過したとしてそれを分とか秒に直すと -> 02:15:200
-            function updateTimetText() {
-
-                //m(分) = 135200 / 60000ミリ秒で割った数の商　-> 2分
-                var m = Math.floor(elapsedTime / 60000);
-
-                //s(秒) = 135200 % 60000ミリ秒で / 1000 (ミリ秒なので1000で割ってやる) -> 15秒
-                var s = Math.floor(elapsedTime % 60000 / 1000);
-
-                //ms(ミリ秒) = 135200ミリ秒を % 1000ミリ秒で割った数の余り
-                var ms = elapsedTime % 1000;
-
-
-                //HTML 上で表示の際の桁数を固定する　例）3 => 03　、 12 -> 012
-                //javascriptでは文字列数列を連結すると文字列になる
-                //文字列の末尾2桁を表示したいのでsliceで負の値(-2)引数で渡してやる。
-                m = ('0' + m).slice(-2);
-                s = ('0' + s).slice(-2);
-                ms = ('0' + ms).slice(-3);
-
-                //HTMLのid　timer部分に表示させる　
-                timer.textContent = m + ':' + s + ':' + ms;
+            if (toggleButton == Status.TIMERSTART) { // Startボタンを押した
+                timerStart = new Date(); // スタート時間を退避
+                toggleButton = 1;
+                document.myForm.myFormButton.value = "Stop!";
+                timerInterval = setInterval("showTime()", 1);
+            } else { // Stopボタンを押した
+                showTime();
+                toggleButton = 0;
+                document.myForm.myFormButton.value = "Start";
+                clearInterval(timerInterval);
             }
+        }
 
+        // 現在の、通算ミリ秒を返す。
+        function totalSeconds() {
+            progressTime = new Date();
+            return progressTime.getTime() - timerStart.getTime();
+        }
 
-            //再帰的に使える用の関数
-            function countUp() {
+        // 計測中の"時"を返す。
+        function getHour(currentTime) {
+            return Math.floor(currentTime / (60 * 60 * 1000));
+        }
 
-                //timerId変数はsetTimeoutの返り値になるので代入する
-                timerId = setTimeout(function () {
+        // 計測中の"分"を返す。
+        function getMinutes(hour) {
+            currentTime = currentTime - (hour * 60 * 60 * 1000);
+            return Math.floor(currentTime / (60 * 1000));
+        }
 
-                    //経過時刻は現在時刻をミリ秒で示すDate.now()からstartを押した時の時刻(startTime)を引く
-                    elapsedTime = Date.now() - startTime + timeToadd;
-                    updateTimetText()
+        // 計測中の"秒"を返す。
+        function getSeconds(minutes) {
+            currentTime = currentTime - (minutes * 60 * 1000);
+            return Math.floor(currentTime / 1000);
+        }
 
-                    //countUp関数自身を呼ぶことで10ミリ秒毎に以下の計算を始める
-                    countUp();
+        // 計測中の"ミリ秒"を返す。
+        function getMillisecond(currentTime) {
+            return currentTime % 1000;
+        }
 
-                    //1秒以下の時間を表示するために10ミリ秒後に始めるよう宣言
-                }, 10);
-            }
+        // フォームに計測時間を表示する。
+        function showTime() {
+            currentTime = totalSeconds();
+            hour        = getHour(currentTime);
+            minutes     = getMinutes(hour);
+            seconds     = getSeconds(minutes);
+            millisecond = getMillisecond(currentTime);
 
-            //startボタンにクリック時のイベントを追加(タイマースタートイベント)
-            start.addEventListener('click', function () {
-
-                //在時刻を示すDate.nowを代入
-                startTime = Date.now();
-
-                //再帰的に使えるように関数を作る
-                countUp();
-            });
-
-            //stopボタンにクリック時のイベントを追加(タイマーストップイベント)
-            stop.addEventListener('click', function () {
-
-                //タイマーを止めるにはclearTimeoutを使う必要があり、そのためにはclearTimeoutの引数に渡すためのタイマーのidが必要
-                clearTimeout(timerId);
-
-
-                //タイマーに表示される時間elapsedTimeが現在時刻かたスタートボタンを押した時刻を引いたものなので、
-                //タイマーを再開させたら0になってしまう。elapsedTime = Date.now - startTime
-                //それを回避するためには過去のスタート時間からストップ時間までの経過時間を足してあげなければならない。elapsedTime = Date.now - startTime + timeToadd (timeToadd = ストップを押した時刻(Date.now)から直近のスタート時刻(startTime)を引く)
-                timeToadd += Date.now() - startTime;
-            });
-
-            //resetボタンにクリック時のイベントを追加(タイマーリセットイベント)
-            reset.addEventListener('click', function () {
-
-                //経過時刻を更新するための変数elapsedTimeを0にしてあげつつ、updateTimetTextで0になったタイムを表示。
-                elapsedTime = 0;
-
-                //リセット時に0に初期化したいのでリセットを押した際に0を代入してあげる
-                timeToadd = 0;
-
-                //updateTimetTextで0になったタイムを表示
-                updateTimetText();
-
-            });
-        })();
+            document.myForm.myFormTime.value = hour + ":" + minutes + ":" + seconds + ":" + millisecond;
+        }
 
     </script>
 </body>
